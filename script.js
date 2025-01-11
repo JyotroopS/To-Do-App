7231ac2731fe4d1d018192568cf06958c9b0ebd0
Tushar Verma <123294028+gitboytushar@users.noreply.github.com>
2025-01-11 23:09:32 +0530

    Upgraded UX and design improvements by Tushar

--- script.js
+++ script.js
@@ -1,56 +1,58 @@
-const inputBox= document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
+const inputBox = document.getElementById('input-box')
+const listContainer = document.getElementById('list-container')
 
 // function to add task
-function addTask(){
-    if (inputBox.value === ''){
-        alert("You must write something!");
-    }
-    else {
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
+function addTask () {
+  if (inputBox.value === '') {
+    alert('You must write something!')
+  } else {
+    let li = document.createElement('li')
+    li.innerHTML = inputBox.value
+    listContainer.appendChild(li)
 
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }
+    let span = document.createElement('span')
+    span.innerHTML = '\u00d7'
+    li.appendChild(span)
+  }
 
-    inputBox.value = '';
-    saveData();
+  inputBox.value = ''
+  saveData()
 }
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData();
+listContainer.addEventListener(
+  'click',
+  function (e) {
+    if (e.target.tagName === 'LI') {
+      e.target.classList.toggle('checked')
+      saveData()
+    } else if (e.target.tagName === 'SPAN') {
+      e.target.parentElement.remove()
+      saveData()
     }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        saveData();
-    }
-},false);
+  },
+  false
+)
 
 // function to save data
-function saveData() {
-    localStorage.setItem("data", listContainer.innerHTML);
+function saveData () {
+  localStorage.setItem('data', listContainer.innerHTML)
 }
 
 // function to show previous data whenever the browser is opened
-function showTask() {
-    listContainer.innerHTML = localStorage.getItem("data");
+function showTask () {
+  listContainer.innerHTML = localStorage.getItem('data')
 }
 
-showTask();
+showTask()
 
-const taskList = document.getElementById('taskList');
+const taskList = document.getElementById('taskList')
 
 // Event listener for the "ADD" button
-addButton.addEventListener('click', addTask);
+addButton.addEventListener('click', addTask)
 
 // Event listener for pressing "Enter" key
-inputBox.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-        addTask();
-    }
-});
+inputBox.addEventListener('keydown', event => {
+  if (event.key === 'Enter') {
+    addTask()
+  }
+})
